7f6205d0e818677808b04c0ebaa8ab15aec60f5c
David Riediger <x128@gmx.at>
2019-10-08 14:47:35 +0200

    Nice shop :)

--- Inter Ex Shopping/script.js
+++ Inter Ex Shopping/script.js
@@ -16,6 +16,13 @@ $("document").ready(function(){
 			update();
 	}
 
+	$("#showcart").click(function(){
+		$("#cart").toggle();
+	})
+
+});
+
+	var sum;
 	function update(){
 		var html ="";
 		var rawsum = 0;
@@ -24,22 +31,38 @@ $("document").ready(function(){
 			if (data[i].quantity > 0) {
 				rawsum += data[i].quantity*data[i].price;
 				html += `<div><img src=${data[i].image}><div><p>${data[i].name}</p><h5>${data[i].price}€</h5></div>
-				<div><input type="number" name=${i} value=${data[i].quantity}><p>${data[i].quantity*data[i].price}€</p></div></div>`
+				<div><input type="number" value=${data[i].quantity} onchange="changeAmount(${i},this.value)"><p>${(data[i].quantity*data[i].price).toFixed(2)}€</p></div></div>`
 			}
 		}
 		tax = rawsum*.22;
-		shipping = 0;
-		discountPct = (rawsum < 40) ? 0 : (rawsum < 100) ? .07 : .12;
+		shipping = (rawsum > 80) ? 2 : 9;
+		discountPct = (rawsum+shipping < 40) ? 0 : (rawsum+shipping < 100) ? .07 : .12;
+		discountDelta = rawsum*discountPct;
+		sum = rawsum - discountDelta + shipping;
 
-
+		html+=`<div><div class="pct70"><p>sub total: ${rawsum.toFixed(2)}€</p><p>tax (22%): ${tax.toFixed(2)}€</p>
+		<p>Discount: -${discountDelta.toFixed(2)}€</p><p>Shipping: ${shipping.toFixed(2)}€</p><hr><h4>Total: ${sum.toFixed(2)}€</h4></div>
+		<div><br><button id="reset" onclick="clearCart()">clear cart</button><br><button id="buy" onclick="alertBuy()">BUY NOW</button></div></div>`
 
 		$("#cart").html(html);
 	}
 
+	function changeAmount(i,amount) {
+		data[i].quantity = amount;
+		update();
+	}
 
+	function clearCart() {
+		for (let i = 0; i < data.length; i++) {
+			data[i].quantity = 0;
+			$("#cart").html("Your cart is empty.<br>Start buying our awesome items now and get a discount!");
+		}
+	}
 
+	function alertBuy(){
+		alert("Are you sure? Total: "+ sum.toFixed(2) + "€")
+	}
 
-});
 // End of JQuery Code
 
 /*Shipping info - for items that cost more than 80 Euros, shipping costs 6 Euros, otherwise it costs 9 Euros
